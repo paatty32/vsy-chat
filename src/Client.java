4723d537423e29189389297a2abe4ee6c26ba11b
paatty32 <patrick.boadu.boafo@hotmail.de>
2020-12-03 13:58:54 +0100

    Einfache Server/Client Klasse

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,35 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+
+    public static void main (String [] args){
+        try{
+            Socket socket = new Socket("localhost", 2000);
+            System.out.println("Verbindung war erfolgreich.");
+
+            //Für die Daten die der Client vom Server erhält
+            InputStreamReader socketStream = new InputStreamReader(socket.getInputStream());
+            BufferedReader in = new BufferedReader(socketStream);
+
+
+            //Damit wird zum server geschrieben.
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("Hallo Server");
+            writer.flush();
+
+            String line;
+            while((line = in.readLine()) != null){
+                System.out.println("Client: gelesen vom Server= " + line);
+
+
+            }
+
+            in.close();
+        } catch (IOException ex){
+            System.out.println("Konnte Socket nicht lesen/schreiben");
+            ex.printStackTrace();
+        }
+    }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,53 @@
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class Server implements Runnable{
+
+    public static void main(String args[]) {
+        int port = 2000;
+        try {
+            ServerSocket serSocket = new ServerSocket(port);
+            System.out.println("Server lauscht auf Port: " + port);
+            while(true){
+                Socket clientSocket = serSocket.accept();
+                Thread t = new Thread(new Server(clientSocket));
+                t.start();
+            }
+        } catch (IOException ex) {
+            System.out.println("Socket konnte nicht gehorcht werden.");
+            ex.printStackTrace();
+        }
+
+    }
+    private Socket clientSocket;
+    private Server(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            InputStreamReader socketStream = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader in = new BufferedReader(socketStream);
+            System.out.println("TEST");
+            //PrintStream stream = System.out;
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+
+            String line;
+            while((line = in.readLine()) != null){
+                System.out.println("TEST2");
+
+                System.out.println("Server: gelsen vom Client= "+line);
+                writer.println(line);
+                writer.flush();
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
